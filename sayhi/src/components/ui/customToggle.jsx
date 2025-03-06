574639a8c2e1c0a35faa1dea92e569c83efe6b1b
Rajesh <rajeshranjan8271@gmail.com>
2025-03-06 06:53:28 +0530

    modified toggle

--- sayhi/src/components/ui/customToggle.jsx
+++ sayhi/src/components/ui/customToggle.jsx
@@ -4,7 +4,7 @@ const CustomToggle = ({ label }) => {
       <span className="ms-3">{label}</span>
       <label className="flex items-center cursor-pointer">
         <input type="checkbox" value="" className="sr-only peer" />
-        <div className="peer peer-checked:bg-green-600 after:top-[2px] after:absolute relative bg-neutral-700 after:bg-white after:border border-neutral-600 peer-checked:after:border-white rounded-full after:rounded-full peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-neutral-300 w-9 after:w-4 h-5 after:h-4 after:content-[''] after:transition-all rtl:peer-checked:after:-translate-x-full peer-checked:after:translate-x-full after:start-[2px]"></div>
+        <div className="peer peer-checked:bg-green-600 after:top-[2px] after:absolute relative bg-neutral-700 after:bg-white after:border border-neutral-600 peer-checked:after:border-white rounded-full after:rounded-full peer-focus:outline-none peer-focus:ring-1 peer-focus:ring-neutral-300 w-9 after:w-4 h-5 after:h-4 after:content-[''] after:transition-all rtl:peer-checked:after:-translate-x-full peer-checked:after:translate-x-full after:start-[2px]"></div>
       </label>
     </div>
   );
