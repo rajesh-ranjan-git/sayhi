faf1037cdce4d4d328b8cecfaed6ae81d410e061
Rajesh <rajeshranjan8271@gmail.com>
2025-03-14 04:05:22 +0530

    Favorites list context

--- sayhi/src/components/call/callListFavorites.tsx
+++ sayhi/src/components/call/callListFavorites.tsx
@@ -1,18 +1,44 @@
 import { UserRoundPlus } from "lucide-react";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import FavoritesSearchbar from "@/components/searchbar/favoritesSearchbar";
+import FavoriteListItem from "@/components/call/favoriteListItem";
 
 const CallListFavorites = () => {
   return (
     <div className="flex flex-col items-center gap-2 p-2 pt-0 border-neutral-800 border-y-3 w-full">
       <div className="w-full">
         <span className="px-2 text-white text-xs">Favorites</span>
-        <div className="flex items-center gap-3 hover:bg-neutral-600 p-2 py-2 rounded-md w-full text-neutral-200 transition-all ease-in-out cursor-pointer">
-          <div className="flex justify-center items-center bg-neutral-800 p-3 rounded-full cursor-pointer">
-            <UserRoundPlus size={20} />
-          </div>
-          <div className="w-full font-semibold">
-            <span>Add Favorite</span>
-          </div>
-        </div>
+        <Popover>
+          <PopoverTrigger className="flex items-center gap-3 hover:bg-neutral-600 p-2 py-2 rounded-md w-full text-neutral-200 transition-all ease-in-out cursor-pointer">
+            <div className="flex justify-center items-center bg-neutral-800 p-3 rounded-full cursor-pointer">
+              <UserRoundPlus size={20} />
+            </div>
+            <div className="w-full font-semibold text-left">
+              <span>Add Favorite</span>
+            </div>
+          </PopoverTrigger>
+          <PopoverContent className="bg-neutral-800 p-0 border-0 rounded-md w-60">
+            <div className="flex flex-col justify-between p-1 rounded-md h-full text-white text-sm">
+              <div className="pt-2 pl-2">
+                <h1 className="font-semibold text-lg">Add Favorites</h1>
+              </div>
+              <FavoritesSearchbar />
+              <div className="rounded-md w-full text-neutral-600 text-xs">
+                <div className="flex items-center gap-2 p-2 w-full transition-all ease-in-out">
+                  <span>Frequently contacted</span>
+                </div>
+              </div>
+              <FavoriteListItem />
+              <FavoriteListItem />
+              <FavoriteListItem />
+              <FavoriteListItem />
+            </div>
+          </PopoverContent>
+        </Popover>
       </div>
     </div>
   );
---
+++ sayhi/src/components/call/favoriteListItem.jsx
@@ -0,0 +1,29 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Checkbox } from "../ui/checkbox";
+
+const FavoriteListItem = () => {
+  return (
+    <div className="flex items-center gap-3 hover:bg-neutral-600 p-2 py-2 rounded-md w-full transition-all ease-in-out cursor-pointer">
+      <div className="cursor-pointer">
+        <Avatar className="w-8 h-8">
+          <AvatarImage src="https://github.com/shadcn.png" />
+          <AvatarFallback>RR</AvatarFallback>
+        </Avatar>
+      </div>
+      <div className="flex flex-col w-full">
+        <div className="flex justify-between items-center w-full">
+          <span className="font-semibold">Rajesh Ranjan</span>
+        </div>
+        <div className="flex items-center gap-2 w-full text-xs">
+          <span>Outgoing</span>
+        </div>
+      </div>
+      <Checkbox
+        id="fav"
+        className="data-[state=checked]:bg-green-400 data-[state=checked]:border-green-400"
+      />
+    </div>
+  );
+};
+
+export default FavoriteListItem;
